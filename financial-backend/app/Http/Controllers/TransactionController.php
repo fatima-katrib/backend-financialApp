4900268694eada0d3b3c2f56815786de767987b4
fatima-katrib <fatimakatrib19@gmail.com>
2023-03-16 07:01:17 +0200

    fixed all transactuons controller

--- financial-backend/app/Http/Controllers/TransactionController.php
+++ financial-backend/app/Http/Controllers/TransactionController.php
@@ -11,35 +11,47 @@ class TransactionController extends Controller
     public function getAllTransactions(Request $request)
     {
         $pagination = $request->input('pagination') ?? 2;
-        $transactions = Recurring::all()->merge(FixedTransaction::all())->with('currency', 'category')->orderBy('start_date', 'desc')->paginate($pagination);
-
-
-        //*********querry currency********* */
+        $transactions = Recurring::with('currency', 'category')
+            ->orderBy('start_date', 'desc')
+            ->union(FixedTransaction::select('id', 'amount', 'start_date', 'schedule', 'is_paid', 'next_payment_date', 'currency_id', 'category_id', 'updated_at', 'created_at'))
+            ->with('currency', 'category')
+            ->orderBy('start_date', 'desc')
+            ->paginate($pagination);
+    
+        //*********query currency********* */
         if ($request->has('currency')) {
             $currency = $request->input('currency');
-            $transactions = Recurring::whereHas('currency', function ($query) use ($currency) {
-                $query->where('name', $currency);
-            })->merge(FixedTransaction::whereHas('currency', function ($query) use ($currency) {
-                $query->where('name', $currency);
-            }))->orderBy('start_date', 'desc')->paginate($pagination);
+            $transactions = Recurring::with('currency', 'category')
+                ->whereHas('currency', function ($query) use ($currency) {
+                    $query->where('name', $currency);
+                })
+                ->union(FixedTransaction::with('currency', 'category')->whereHas('currency', function ($query) use ($currency) {
+                    $query->where('name', $currency);
+                }))
+                ->orderBy('start_date', 'desc')
+                ->paginate($pagination);
         }
-
-        //*********querry income/outcome********* */
+    
+        //*********query income/outcome********* */
         if ($request->has('type_code')) {
             $type_code = $request->input('type_code');
-            // $week = $request->input('week');
-            // $month = $request->input('month');
-            // $year = $request->input('year');
-            $transactions = Recurring::whereHas('category', function ($query) use ($type_code) {
-                $query->where('type_code', $type_code);
-            })->merge(FixedTransaction::whereHas('category', function ($query) use ($type_code) {
-                $query->where('type_code', $type_code);
-            }))->orderBy('start_date', 'desc')->paginate($pagination);
-
-            return response()->json([
-                'status' => 201,
-                'message' => "transactions",
-                'data' => $transactions
-            ]);
+            $transactions = Recurring::with('currency', 'category')
+                ->whereHas('category', function ($query) use ($type_code) {
+                    $query->where('type_code', $type_code);
+                })
+                ->union(FixedTransaction::with('currency', 'category')->whereHas('category', function ($query) use ($type_code) {
+                    $query->where('type_code', $type_code);
+                }))
+                ->orderBy('start_date', 'desc')
+                ->paginate($pagination);
         }
-    }}
+    
+        return response()->json([
+            'status' => 201,
+            'message' => "transactions",
+            'data' => $transactions
+        ]);
+    }
+    
+    
+}
